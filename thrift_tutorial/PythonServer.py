2c6e08694cea1ecb54598bc0fb0a4e90a64d1efe
sebhmg <sebastienh@mirageoscience.com>
2019-09-12 00:52:15 -0400

    extract CalculatorHandler to dedicated file

--- thrift_tutorial/PythonServer.py
+++ thrift_tutorial/PythonServer.py
@@ -23,6 +23,7 @@ import glob
 import sys
 sys.path.append('gen-py')
 
+import CalculatorHandler
 from tutorial import Calculator
 from tutorial.ttypes import InvalidOperation, Operation
 
@@ -33,57 +34,8 @@ from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
 
-
-class CalculatorHandler:
-    def __init__(self):
-        self.log = {}
-
-    def ping(self):
-        print('ping()')
-
-    def add(self, n1, n2):
-        print('add(%d,%d)' % (n1, n2))
-        return n1 + n2
-
-    def calculate(self, logid, work):
-        print('calculate(%d, %r)' % (logid, work))
-
-        if work.op == Operation.ADD:
-            val = work.num1 + work.num2
-        elif work.op == Operation.SUBTRACT:
-            val = work.num1 - work.num2
-        elif work.op == Operation.MULTIPLY:
-            val = work.num1 * work.num2
-        elif work.op == Operation.DIVIDE:
-            if work.num2 == 0:
-                x = InvalidOperation()
-                x.whatOp = work.op
-                x.why = 'Cannot divide by 0'
-                raise x
-            val = work.num1 / work.num2
-        else:
-            x = InvalidOperation()
-            x.whatOp = work.op
-            x.why = 'Invalid operation'
-            raise x
-
-        log = SharedStruct()
-        log.key = logid
-        log.value = '%d' % (val)
-        self.log[logid] = log
-
-        return val
-
-    def getStruct(self, key):
-        print('getStruct(%d)' % (key))
-        return self.log[key]
-
-    def zip(self):
-        print('zip()')
-
-
 if __name__ == '__main__':
-    handler = CalculatorHandler()
+    handler = CalculatorHandler.CalculatorHandler()
     processor = Calculator.Processor(handler)
     transport = TSocket.TServerSocket(host='127.0.0.1', port=9090)
     tfactory = TTransport.TBufferedTransportFactory()
