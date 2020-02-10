913739863d18386c747ab293e3e9dfa061551aba
domfournier <dominiquef@mirageoscience.com>
2020-02-10 09:10:36 -0800

    Fix FloatData doc strings

--- geoh5io/data/float_data.py
+++ geoh5io/data/float_data.py
@@ -4,12 +4,19 @@ from .data import Data, PrimitiveTypeEnum
 
 
 class FloatData(Data):
+    """
+    Data container for floats values
+    """
+
     @classmethod
     def primitive_type(cls) -> PrimitiveTypeEnum:
         return PrimitiveTypeEnum.FLOAT
 
     @property
     def values(self) -> ndarray[float]:
+        """
+        :return: values: An array of values
+        """
         if (getattr(self, "_values", None) is None) and self.existing_h5_entity:
             self._values = self.workspace.fetch_values(self.uid)
 
@@ -17,17 +24,6 @@ class FloatData(Data):
 
     @values.setter
     def values(self, values):
-        """
-        values(values)
-
-        Assign array of float as values
-
-        Parameters
-        ----------
-        values: numpy.ndarray
-            Array of floats
-
-        """
         self.update_h5 = "values"
         self._values = ravel(values)
 
