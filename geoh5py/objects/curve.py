e63d2c9730e5fd7239f5a3c5afa7d123bbf6c113
Dominique Fournier <dominiquef@mirageoscience.com>
2020-04-22 13:47:27 -0700

    Implement curve parts property

--- geoh5py/objects/curve.py
+++ geoh5py/objects/curve.py
@@ -1,7 +1,7 @@
 import uuid
-from typing import Optional
+from typing import List, Optional, Union
 
-from numpy import arange, c_, ndarray, unique, zeros
+from numpy import arange, asarray, c_, ndarray, unique, vstack, where, zeros
 
 from .object_base import ObjectType
 from .points import Points
@@ -20,7 +20,7 @@ class Curve(Points):
     def __init__(self, object_type: ObjectType, **kwargs):
 
         self._cells: Optional[ndarray] = None
-        self._line_id: Optional[ndarray] = None
+        self._parts: Optional[ndarray] = None
         super().__init__(object_type, **kwargs)
 
         if object_type.name == "None":
@@ -33,7 +33,14 @@ class Curve(Points):
         numpy.ndarray of int, shape ("*", 2)
         """
         if getattr(self, "_cells", None) is None:
-            if self.existing_h5_entity:
+            if self._parts is not None:
+                cells = []
+                for part_id in self.unique_parts:
+                    ind = where(self.parts == part_id)[0]
+                    cells.append(c_[ind[:-1], ind[1:]])
+                self._cells = vstack(cells)
+
+            elif self.existing_h5_entity:
                 self._cells = self.workspace.fetch_cells(self.uid)
             else:
                 if self.vertices is not None:
@@ -49,38 +56,52 @@ class Curve(Points):
         assert indices.dtype == "uint32", "Indices array must be of type 'uint32'"
         self.modified_attributes = "cells"
         self._cells = indices
-        self._line_id = None
+        self._parts = None
 
     @property
-    def line_id(self):
+    def parts(self):
         """
-        Connections of cells forming unique line_id:
-        list of numpy.ndarray of int, shape ("*", 2)
+        Line identifier: numpy.ndarray of int, shape (n_vertices, )
         """
-        if getattr(self, "_line_id", None) is None and self.cells is not None:
+        if getattr(self, "_parts", None) is None and self.cells is not None:
 
             cells = self.cells
-            line_id = zeros(self.cells.shape[0], dtype="int")
+            parts = zeros(self.vertices.shape[0], dtype="int")
             count = 0
             for ind in range(1, cells.shape[0]):
 
                 if cells[ind, 0] != cells[ind - 1, 1]:
                     count += 1
 
-                line_id[ind] = count
+                parts[cells[ind, :]] = count
 
-            self._line_id = line_id
+            self._parts = parts
 
-        return self._line_id
+        return self._parts
+
+    @parts.setter
+    def parts(self, indices: Union[List, ndarray]):
+        if self.vertices is not None:
+            if isinstance(indices, list):
+                indices = asarray(indices)
+
+            assert indices.dtype == int, "Indices array must be of type 'uint32'"
+            assert indices.shape == (
+                self.vertices.shape[0],
+            ), f"Provided parts must be of shape {self.vertices.shape[0]}"
+
+            self.modified_attributes = "cells"
+            self._parts = indices
+            self._cells = None
 
     @property
-    def unique_lines(self):
+    def unique_parts(self):
         """
-        Unique lines connected by cells
+        Unique parts connected by cells
         """
-        if self.line_id is not None:
+        if self.parts is not None:
 
-            return unique(self.line_id).tolist()
+            return unique(self.parts).tolist()
 
         return None
 
