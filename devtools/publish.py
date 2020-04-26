026d821a14ec9ad2a22305eecd2380df950a757b
sebhmg <sebastienh@mirageoscience.com>
2020-04-26 00:50:53 -0400

    GEOIO-26: fix formatting of publish script
    also exclude it from pylint

--- devtools/publish.py
+++ devtools/publish.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
-import geoh5py
 from poetry_publish.publish import poetry_publish
+
+import geoh5py
 
 
 def publish():
     poetry_publish(
-        package_root=Path(geoh5py.__file__).parent.parent,
-        version=geoh5py.__version__,
+        package_root=Path(geoh5py.__file__).parent.parent, version=geoh5py.__version__,
     )
