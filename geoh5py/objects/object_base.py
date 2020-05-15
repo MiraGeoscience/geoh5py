8c8d2829a84cd47d546353e766a0dddcc3e55e60
Dominique Fournier <dominiquef@mirageoscience.com>
2020-05-14 17:51:23 -0700

    Update docstrings for object_base

--- geoh5py/objects/object_base.py
+++ geoh5py/objects/object_base.py
@@ -52,212 +52,27 @@ class ObjectBase(Entity):
 
         super().__init__(**kwargs)
 
-    @property
-    def last_focus(self) -> str:
-        """
-        Object visible in camera on start: bool
-        """
-        return self._last_focus
-
-    @last_focus.setter
-    def last_focus(self, value: str):
-        self._last_focus = value
-
-    @property
-    def entity_type(self) -> ObjectType:
-        """
-        Object type: EntityType
-        """
-        return self._entity_type
-
-    @property
-    def vertices(self):
-        """
-        Vertices
-        """
-        return None
-
-    @property
-    def cells(self):
-        """
-        Cells
-        """
-        return None
-
-    @property
-    def faces(self):
-        """
-        Faces
-        """
-        return None
-
-    @property
-    def n_vertices(self) -> Optional[int]:
-        """
-        Number of vertices
-
-        :return: Number of vertices
-        """
-        if self.vertices is not None:
-            return self.vertices.shape[0]
-        return None
-
-    @property
-    def n_cells(self) -> Optional[int]:
-        """
-        n_cells
-
-        Returns
-        -------
-        n_cells: int
-            Number of cells
-        """
-        if self.cells is not None:
-            return self.cells.shape[0]
-        return None
-
-    @property
-    def property_groups(self) -> List[PropertyGroup]:
-        """
-        List of property (data) groups: List[PropertyGroup]
-        """
-        return self._property_groups
-
-    @property_groups.setter
-    def property_groups(self, prop_groups: List[PropertyGroup]):
-        # Check for existing property_group
-        for prop_group in prop_groups:
-            if not any(
-                [pg.uid == prop_group.uid for pg in self.property_groups]
-            ) and not any([pg.name == prop_group.name for pg in self.property_groups]):
-                prop_group.parent = self
-
-                self.modified_attributes = "property_groups"
-                self._property_groups = self.property_groups + [prop_group]
-
-    @classmethod
-    def find_or_create_type(
-        cls, workspace: "workspace.Workspace", **kwargs
-    ) -> ObjectType:
-        """
-        Find or create a type for a given object class
-
-        :param Current workspace: Workspace
-
-        :return: A new or existing object type
-        """
-        return ObjectType.find_or_create(workspace, cls, **kwargs)
-
-    @classmethod
-    @abstractmethod
-    def default_type_uid(cls) -> uuid.UUID:
-        ...
-
-    def add_data_to_group(
-        self, data: Union[List, Data, uuid.UUID, str], name: str
-    ) -> PropertyGroup:
-        """
-        Append data to a property group where the data can be a Data object, its name
-        or uid. The given group identifier (name or uid) is created if it does not exist already.
-        All given data must be children of the object.
-
-        :param data: Data object or uuid of data
-        :param name: Name of a property group. A new group is created
-            if none exist with the given name.
-
-        :return property_group: The target property_group
-        """
-        # prop_group = self.get_property_group(name)
-        #
-        # if prop_group is None:
-        prop_group = self.find_or_create_property_group(name=name)
-
-        children_uid = [child.uid for child in self.children]
-
-        def reference_to_uid(value):
-            if isinstance(value, Data):
-                uid = [value.uid]
-            elif isinstance(value, str):
-                uid = [
-                    obj.uid
-                    for obj in self.workspace.get_entity(value)
-                    if obj.uid in children_uid
-                ]
-            elif isinstance(value, uuid.UUID):
-                uid = [value]
-            return uid
-
-        if isinstance(data, list):
-            uid = []
-            for datum in data:
-                uid += reference_to_uid(datum)
-        else:
-            uid = reference_to_uid(data)
-
-        for i in uid:
-            assert i in [
-                child.uid for child in self.children
-            ], f"Given data with uuid {i} does not match any known children"
-
-        prop_group.properties = uid
-        self.modified_attributes = "property_groups"
-
-        return prop_group
-
-    def find_or_create_property_group(self, **kwargs) -> PropertyGroup:
-        """
-        Create property groups from given group names and properties.
-        An existing property_group is returned if one exists with the same name.
-
-        :param kwargs: Any arguments taken by the PropertyGroup class
-
-        :return: A new or existing property_group object
-        """
-        prop_group = PropertyGroup(**kwargs)
-        if any([pg.name == prop_group.name for pg in self.property_groups]):
-            prop_group = [
-                pg for pg in self.property_groups if pg.name == prop_group.name
-            ][0]
-        else:
-            self.property_groups = [prop_group]
-
-        return prop_group
-
-    def get_property_group(
-        self, group_id: Union[str, uuid.UUID]
-    ) -> Optional[PropertyGroup]:
-        """
-        Retrieve a property_group from one of its identifier, either by name or uuid
-
-        :param group_id: PropertyGroup identifier, either name or uuid
-
-        :return: PropertyGroup with the given name
-        """
-        if isinstance(group_id, uuid.UUID):
-            groups_list = [pg for pg in self.property_groups if pg.uid == group_id]
-
-        else:  # Extract all groups uuid with matching group_id
-            groups_list = [pg for pg in self.property_groups if pg.name == group_id]
-
-        try:
-            prop_group = groups_list[0]
-        except IndexError:
-            print(f"No property_group {group_id} found.")
-            return None
-
-        return prop_group
-
     def add_data(
         self, data: dict, property_group: str = None
     ) -> Union[Data, List[Data]]:
         """
-        Create data with association from dictionary of data objects name and values.
-        The provided values can either be a dictionary of kwargs accepted by the target
-        Data object class or an array of values. If not provided as argument, a data
-        association type is assigned based on the length of the given values.
+        Create :obj:`~geoh5py.data.data.Data` from dictionary of name and arguments.
+        The provided arguments can be any property of the target class.
 
-        :param data: Dictionary of data to be added to the object,
-            e.g. data = {"data_name1": {'values', values1, 'association': 'VERTEX', ...}, ...}
+        :param data: Dictionary of data to be added to the object, e.g.
+
+        .. code-block:: python
+
+            data = {
+                "data_A": {
+                    'values', [v_1, v_2, ...],
+                    'association': 'VERTEX'
+                    },
+                "data_B": {
+                    'values', [v_1, v_2, ...],
+                    'association': 'CELLS'
+                    },
+            }
 
         :return data_list: List of new Data objects
         """
@@ -320,9 +135,110 @@ class ObjectBase(Entity):
 
         return data_objects
 
+    def add_data_to_group(
+        self, data: Union[List, Data, uuid.UUID, str], name: str
+    ) -> PropertyGroup:
+        """
+        Append data children to a :obj:`~geoh5py.groups.property_group.PropertyGroup`
+        All given data must be children of the parent object.
+
+        :param data: :obj:`~geoh5py.data.data.Data` object,
+            :obj:`~geoh5py.shared.entity.Entity.uid` or
+            :obj:`~geoh5py.shared.entity.Entity.name` of data
+        :param name: Name of a :obj:`~geoh5py.groups.property_group.PropertyGroup`.
+            A new group is created if none exist with the given name.
+
+        :return property_group: The target property_group
+        """
+        prop_group = self.find_or_create_property_group(name=name)
+
+        children_uid = [child.uid for child in self.children]
+
+        def reference_to_uid(value):
+            if isinstance(value, Data):
+                uid = [value.uid]
+            elif isinstance(value, str):
+                uid = [
+                    obj.uid
+                    for obj in self.workspace.get_entity(value)
+                    if obj.uid in children_uid
+                ]
+            elif isinstance(value, uuid.UUID):
+                uid = [value]
+            return uid
+
+        if isinstance(data, list):
+            uid = []
+            for datum in data:
+                uid += reference_to_uid(datum)
+        else:
+            uid = reference_to_uid(data)
+
+        for i in uid:
+            assert i in [
+                child.uid for child in self.children
+            ], f"Given data with uuid {i} does not match any known children"
+
+        prop_group.properties = uid
+        self.modified_attributes = "property_groups"
+
+        return prop_group
+
+    @property
+    def cells(self):
+        ...
+
+    @classmethod
+    @abstractmethod
+    def default_type_uid(cls) -> uuid.UUID:
+        ...
+
+    @property
+    def entity_type(self) -> ObjectType:
+        """
+        :obj:`~geoh5py.shared.entity_type.EntityType`: Object type
+        """
+        return self._entity_type
+
+    @property
+    def faces(self):
+        ...
+
+    @classmethod
+    def find_or_create_type(
+        cls, workspace: "workspace.Workspace", **kwargs
+    ) -> ObjectType:
+        """
+        Find or create a type for a given object class
+
+        :param Current workspace: Workspace
+
+        :return: A new or existing object type
+        """
+        return ObjectType.find_or_create(workspace, cls, **kwargs)
+
+    def find_or_create_property_group(self, **kwargs) -> PropertyGroup:
+        """
+        Find or create :obj:`~geoh5py.groups.property_group.PropertyGroup`
+        from given name and properties.
+
+        :param kwargs: Any arguments taken by the PropertyGroup class
+
+        :return: A new or existing :obj:`~geoh5py.groups.property_group.PropertyGroup`
+        """
+        prop_group = PropertyGroup(**kwargs)
+        if any([pg.name == prop_group.name for pg in self.property_groups]):
+            prop_group = [
+                pg for pg in self.property_groups if pg.name == prop_group.name
+            ][0]
+        else:
+            self.property_groups = [prop_group]
+
+        return prop_group
+
     def get_data(self, name: str) -> List[Data]:
         """
-        Get data objects by name
+        Get a child :obj:`~geoh5py.data.data.Data` by name.
 
         :param name: Name of the target child data
 
@@ -338,11 +254,89 @@ class ObjectBase(Entity):
 
     def get_data_list(self) -> List[str]:
         """
-        :return: List of names of data associated with the object
+        Get a list of names of all children :obj:`~geoh5py.data.data.Data`.
 
+        :return: List of names of data associated with the object
         """
         name_list = []
         for child in self.children:
             if isinstance(child, Data):
                 name_list.append(child.name)
         return sorted(name_list)
+
+    def get_property_group(
+        self, group_id: Union[str, uuid.UUID]
+    ) -> Optional[PropertyGroup]:
+        """
+        Retrieve a :obj:`~geoh5py.groups.property_group.PropertyGroup` from one of its
+        identifier.
+
+        :param group_id: PropertyGroup identifier, either name or uuid
+
+        :return: PropertyGroup with the given name
+        """
+        if isinstance(group_id, uuid.UUID):
+            groups_list = [pg for pg in self.property_groups if pg.uid == group_id]
+
+        else:  # Extract all groups uuid with matching group_id
+            groups_list = [pg for pg in self.property_groups if pg.name == group_id]
+
+        try:
+            prop_group = groups_list[0]
+        except IndexError:
+            print(f"No property_group {group_id} found.")
+            return None
+
+        return prop_group
+
+    @property
+    def last_focus(self) -> str:
+        """
+        :obj:`bool`: Object visible in camera on start
+        """
+        return self._last_focus
+
+    @last_focus.setter
+    def last_focus(self, value: str):
+        self._last_focus = value
+
+    @property
+    def n_cells(self) -> Optional[int]:
+        """
+        :obj:`int`: Number of cells
+        """
+        if self.cells is not None:
+            return self.cells.shape[0]
+        return None
+
+    @property
+    def n_vertices(self) -> Optional[int]:
+        """
+        :obj:`int`: Number of vertices
+        """
+        if self.vertices is not None:
+            return self.vertices.shape[0]
+        return None
+
+    @property
+    def property_groups(self) -> List[PropertyGroup]:
+        """
+        :obj:`list`: List of property (data) groups.
+        """
+        return self._property_groups
+
+    @property_groups.setter
+    def property_groups(self, prop_groups: List[PropertyGroup]):
+        # Check for existing property_group
+        for prop_group in prop_groups:
+            if not any(
+                [pg.uid == prop_group.uid for pg in self.property_groups]
+            ) and not any([pg.name == prop_group.name for pg in self.property_groups]):
+                prop_group.parent = self
+
+                self.modified_attributes = "property_groups"
+                self._property_groups = self.property_groups + [prop_group]
+
+    @property
+    def vertices(self):
+        ...
