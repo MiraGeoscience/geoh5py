b6a7006f77d98000adb27008f0528d58a70fc0b5
sebastienh <sebastienh@mirageoscience.com>
2022-06-01 18:04:45 -0400

    name it alpha.2

--- geoh5py/__init__.py
+++ geoh5py/__init__.py
@@ -15,4 +15,4 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with geoh5py.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "0.3.0-alpha.1"
+__version__ = "0.3.0-alpha.2"
