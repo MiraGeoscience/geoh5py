355de8cf78c180d0cceaabbf89536b6af2411475
dominiquef <dominiquef@mirageoscience.com>
2022-07-04 12:46:59 -0700

    Make open return self. Augment unit test

--- geoh5py/workspace/workspace.py
+++ geoh5py/workspace/workspace.py
@@ -1011,10 +1011,10 @@ class Workspace(AbstractContextManager):
         """Get all active Object entities registered in the workspace."""
         return self._all_objects()
 
-    def open(self, mode: str | None = None) -> None:
+    def open(self, mode: str | None = None) -> Workspace:
         if isinstance(self._geoh5, h5py.File):
             warnings.warn(f"Workspace already opened in mode {self._geoh5.mode}.")
-            return
+            return self
 
         if mode is None:
             mode = self._mode
@@ -1040,6 +1040,8 @@ class Workspace(AbstractContextManager):
             self._io_call(H5Writer.create_geoh5, self, mode="a")
 
         self.fetch_or_create_root()
+
+        return self
 
     def _register_type(self, entity_type: EntityType):
         weakref_utils.insert_once(self._types, entity_type.uid, entity_type)
