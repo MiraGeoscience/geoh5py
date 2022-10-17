649bf1f96590e4e9843d3e9f53de5f38268614a4
dominiquef <dominiquef@mirageoscience.com>
2022-10-17 15:23:29 -0700

    Create "0.5.0-alpha.1" release

--- geoh5py/__init__.py
+++ geoh5py/__init__.py
@@ -15,4 +15,4 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with geoh5py.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "0.4.0"
+__version__ = "0.5.0-alpha.1"
