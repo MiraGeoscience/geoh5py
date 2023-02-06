9f09f893269574a08386a6c7cb6b183e69e1e75d
dominiquef <dominiquef@mirageoscience.com>
2023-02-06 10:43:51 -0500

    Don't require entity_type

--- geoh5py/workspace/workspace.py
+++ geoh5py/workspace/workspace.py
@@ -345,7 +345,6 @@ class Workspace(AbstractContextManager):
             )
 
         if recovered_entity is not None:
-            self.save_entity_type(recovered_entity.entity_type)
             recovered_entity.on_file = True
             recovered_entity.entity_type.on_file = True
 
