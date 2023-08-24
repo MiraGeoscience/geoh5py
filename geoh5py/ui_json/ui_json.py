f4caf2a39ae20f63860640c805c6a1dd631ffca0
benjamink <benjamink@mirageoscience.com>
2023-08-24 09:04:32 -0700

    ..guess not

--- geoh5py/ui_json/ui_json.py
+++ geoh5py/ui_json/ui_json.py
@@ -17,7 +17,7 @@
 
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Dict
 
 from geoh5py.shared.exceptions import AggregateValidationError, BaseValidationError
 from geoh5py.ui_json.parameters import (
