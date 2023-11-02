578cd0e5a576eee8cd4df4af0d438477b603f5f1
benjamink <benjamink@mirageoscience.com>
2023-11-02 14:11:11 -0700

    old changes to be merged into release/0.8.0

--- geoh5py/shared/utils.py
+++ geoh5py/shared/utils.py
@@ -240,13 +240,17 @@ def clear_array_attributes(entity: Entity, recursive: bool = False):
             clear_array_attributes(child, recursive=recursive)
 
 
-def compare_entities(
+def compare_entities(  # pylint: disable=too-many-branches
     object_a, object_b, ignore: list | None = None, decimal: int = 6
 ) -> None:
     ignore_list = ["_workspace", "_children", "_visual_parameters"]
     if ignore is not None:
         for item in ignore:
             ignore_list.append(item)
+
+    if isinstance(object_a, bytes):
+        assert object_a == object_b, "Bytes values do not match."
+        return
 
     for attr in object_a.__dict__.keys():
         if attr in ignore_list:
