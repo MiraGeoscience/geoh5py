075d737571f671b70aa4aa2b5732de769a638900
benjamink <benjamink@mirageoscience.com>
2024-01-04 10:59:40 -0800

    use code?

--- tests/drillhole_v4_0_test.py
+++ tests/drillhole_v4_0_test.py
@@ -15,7 +15,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with geoh5py.  If not, see <https://www.gnu.org/licenses/>.
 
-# pylint: disable=R0914, cyclic-import
+# pylint: disable=R0914, R0401
 # mypy: ignore-errors
 
 from __future__ import annotations
