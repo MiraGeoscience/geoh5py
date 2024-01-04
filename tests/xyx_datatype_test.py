3de29909e8d46aaf4818a7a9fa2e541d65c988ed
benjamink <benjamink@mirageoscience.com>
2024-01-04 11:12:10 -0800

    ignore cyclic-import

--- tests/xyx_datatype_test.py
+++ tests/xyx_datatype_test.py
@@ -15,6 +15,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with geoh5py.  If not, see <https://www.gnu.org/licenses/>.
 
+# pylint: disable=cyclic-import
 
 from __future__ import annotations
 
