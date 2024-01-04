b2ed5e62bbe17baec3d9994ecb2c26809e5677cb
benjamink <benjamink@mirageoscience.com>
2024-01-04 11:14:35 -0800

    use code

--- tests/xyx_datatype_test.py
+++ tests/xyx_datatype_test.py
@@ -15,7 +15,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with geoh5py.  If not, see <https://www.gnu.org/licenses/>.
 
-# pylint: disable=cyclic-import
+# pylint: disable=R0401
 
 from __future__ import annotations
 
