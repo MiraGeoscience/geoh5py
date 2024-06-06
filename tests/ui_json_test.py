025351fc8cc5893af762ab107c640e30430707a6
Sam Bradley <sam.bradley@csiro.au>
2024-06-06 10:57:23 +0800

    modify test to allow for changed error message wording in python 3.12

--- tests/ui_json_test.py
+++ tests/ui_json_test.py
@@ -540,7 +540,12 @@ def test_input_file(tmp_path: Path):
     ):
         InputFile.read_ui_json("somefile.json")
 
-    with pytest.raises(TypeError, match="expected str, bytes or os.PathLike object"):
+    with pytest.raises(
+        TypeError,
+        match="expected str, bytes or os.PathLike object|"
+        + "argument should be a str or an os.PathLike object where "
+        + "__fspath__ returns a str, not 'int'",
+    ):
         InputFile.read_ui_json(123)
 
     # Load the input back in
