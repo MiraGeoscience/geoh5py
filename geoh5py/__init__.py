3b90863fed3a2cdee18c48e6493b092c37fa0d76
SophieCurinier <sophiec@mirageoscience.com>
2024-08-08 09:18:29 -0400

    [DEVOPS-439] Update hatch dependency

--- geoh5py/__init__.py
+++ geoh5py/__init__.py
@@ -24,25 +24,6 @@ from geoh5py.workspace import Workspace
 from . import groups, objects
 from .groups import CustomGroup
 
-# Define the variable '__version__' at the runtime
-try:
-    from setuptools_scm import get_version
-
-    # This will fail with LookupError if the package is not installed in
-    # editable mode or if Git is not installed.
-    __version__ = get_version(root="..", relative_to=__file__)
-except (ImportError, LookupError):
-    # As a fallback, use the version that is hard-coded in the file.
-    try:
-        from geoh5py._version import __version__  # noqa: F401
-    except ModuleNotFoundError:
-        # The user is probably trying to run this without having installed
-        # the package, so complain.
-        raise RuntimeError(
-            "Hatch VCS Footgun Example is not correctly installed. "
-            "Please install it with pip."
-        )
-
 def get_type_uid_classes():
     members = []
     for _, member in inspect.getmembers(groups) + inspect.getmembers(objects):
