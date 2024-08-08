6a724b2a9e3bc9a7384b6a4ccd4bed414392b7c6
SophieCurinier <sophiec@mirageoscience.com>
2024-08-07 21:42:50 -0400

    [DEVOPS-439] Use vcs to control the package version

--- geoh5py/__init__.py
+++ geoh5py/__init__.py
@@ -17,8 +17,6 @@
 
 # flake8: noqa
 
-__version__ = "0.10.0-alpha.1"
-
 import inspect
 
 from geoh5py.workspace import Workspace
@@ -26,6 +24,24 @@ from geoh5py.workspace import Workspace
 from . import groups, objects
 from .groups import CustomGroup
 
+# Define the variable '__version__' at the runtime
+try:
+    from setuptools_scm import get_version
+
+    # This will fail with LookupError if the package is not installed in
+    # editable mode or if Git is not installed.
+    __version__ = get_version(root="..", relative_to=__file__)
+except (ImportError, LookupError):
+    # As a fallback, use the version that is hard-coded in the file.
+    try:
+        from geoh5py._version import __version__  # noqa: F401
+    except ModuleNotFoundError:
+        # The user is probably trying to run this without having installed
+        # the package, so complain.
+        raise RuntimeError(
+            "Hatch VCS Footgun Example is not correctly installed. "
+            "Please install it with pip."
+        )
 
 def get_type_uid_classes():
     members = []
