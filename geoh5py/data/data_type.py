dc7e8c1f8969259d56005a67ae86bf6dd2972ca6
dominiquef <dominiquef@mirageoscience.com>
2024-08-20 16:32:26 -0700

    Add "cumulative_distribution_function"

--- geoh5py/data/data_type.py
+++ geoh5py/data/data_type.py
@@ -33,7 +33,14 @@ if TYPE_CHECKING:
     from .data import Data  # noqa: F401
 
 
-ColorMapping = Literal["linear", "equal_area", "logarithmic", "cdf", "missing"]
+ColorMapping = Literal[
+    "linear",
+    "equal_area",
+    "logarithmic",
+    "cdf",
+    "cumulative_distribution_function",
+    "missing",
+]
 
 
 class DataType(EntityType):
