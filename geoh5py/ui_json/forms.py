4ffbf393d0d055f54f6ced813f5eb823e39b85b8
benk-mira <81254271+benk-mira@users.noreply.github.com>
2024-09-12 15:37:34 -0700

    Update geoh5py/ui_json/forms.py
    Co-authored-by: domfournier <dominiquef@mirageoscience.com>

--- geoh5py/ui_json/forms.py
+++ geoh5py/ui_json/forms.py
@@ -328,8 +328,7 @@ class DataForm(BaseForm):
             child = self.property
 
         if child is not None:
-            children = params[self.parent].children
-            if not children or child not in [k.uid for k in children]:
+            if not isinstance(params[self.parent], ObjectBase) or params[self.parent].get_entity(child)[0] is None:
                 raise UIJsonError(f"{child} data is not a child of {self.parent}.")
 
     def validate_data(self, params: dict[str, Any]):
