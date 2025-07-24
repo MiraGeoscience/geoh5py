0c0a95b12c963b2cf4288724291e5ed4130f17b4
saicheran <saicheranb@mirageoscience.com>
2025-07-24 16:04:01 -0400

    Reset version numbers to "0.0.0" in version.py, pyproject.toml, and recipe.yaml for consistency and to prepare for the next release cycle. Added version.py to .gitignore to prevent tracking of version files.

--- geoh5py/version.py
+++ geoh5py/version.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.11.0.post103.dev0+50de28c0"
-__version_tuple__ = (0, 11, 0, "post103", "dev0", "50de28c0")
+__version__ = "0.0.0"
+__version_tuple__ = (0, 0, 0)
