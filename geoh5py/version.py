32ab444fd9ce9c2b1b84cb538c99c4f1fdcb7907
saicheran <saicheranb@mirageoscience.com>
2025-07-24 13:13:30 -0400

    Update version to 0.11.0.post103.dev0+50de28c0 in version.py, recipe.yaml, and remove fallback-version from pyproject.toml

--- geoh5py/version.py
+++ geoh5py/version.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.11.0.post102.dev0+eb77d2e5"
-__version_tuple__ = (0, 11, 0, "post102", "dev0", "eb77d2e5")
+__version__ = "0.11.0.post103.dev0+50de28c0"
+__version_tuple__ = (0, 11, 0, "post103", "dev0", "50de28c0")
