e61086e303cbaa19e91d921f6c62e01673710e97
Sébastien Hensgen <24550538+sebhmg@users.noreply.github.com>
2025-09-15 18:44:17 -0400

    [DEVOPS-693] apply albaster then and dynamic config
    add googleanalytics

---
+++ docs/_ext/dynamic_config.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from importlib.metadata import version as get_version
+from packaging.version import Version
+
+
+dated_copyright = f"{datetime.now().strftime('%Y, Mira Geoscience Ltd')}"
+
+def get_copyright_notice():
+    return f"Copyright {dated_copyright}."
+
+
+def setup(app):
+    # The full version, including alpha/beta/rc tags.
+    release = get_version("geoh5py")
+    # The shorter X.Y.Z version.
+    version = Version(release).base_version
+
+    app.config.project_copyright = f"{dated_copyright}"
+    app.config.release = release
+    app.config.version = version
+    app.config.html_theme_options = {
+        "description": f"version {release}",
+        "fixed_sidebar": True,
+        "logo_name": "Geoh5py",
+        "show_relbars": True,
+    }
+    app.config.rst_epilog = f"\n.. |copyright_notice| replace:: {get_copyright_notice()}\n"
+
+    return {"version": "0.1", "parallel_read_safe": True}
