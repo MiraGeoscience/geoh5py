dea0c4d69537321c019c31a5d04183dc19cc6f28
domfournier <dominiquef@mirageoscience.com>
2025-10-14 11:07:43 -0700

    Update geoh5py/shared/utils.py
    Co-authored-by: benk-mira <81254271+benk-mira@users.noreply.github.com>

--- geoh5py/shared/utils.py
+++ geoh5py/shared/utils.py
@@ -1226,7 +1226,8 @@ def dict_to_json_str(data: dict) -> str:
 def uuid_from_values(data: dict | str) -> UUID:
     """
     Create a deterministic uuid of a dictionary or its json string representation.
-    they are first converted to a json string.
+    
+    Floats are formatted to fixed precision scientific notation and objects are converted to uid strings.
 
     :param data: Tuple containing the values of a sweep iteration.
 
