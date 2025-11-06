02bca4ac9ed8a94b965906eb30804e3eea368aa6
benjamink <benjamink@mirageoscience.com>
2025-11-06 11:55:14 -0800

    Docstring typing upgrade

--- geoh5py/ui_json/ui_json.py
+++ geoh5py/ui_json/ui_json.py
@@ -35,6 +35,8 @@ from pydantic import (
 )
 
 from geoh5py import Workspace
+from geoh5py.groups import PropertyGroup
+from geoh5py.shared import Entity
 from geoh5py.shared.utils import fetch_active_workspace
 from geoh5py.shared.validators import empty_string_to_none, none_to_empty_string
 from geoh5py.ui_json.forms import BaseForm
@@ -74,10 +76,12 @@ class BaseUIJson(BaseModel):
     _path: Path | None = None
     _groups: dict[str, list[str]] | None = None
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """Repr level shows a path if it exists or the title otherwise."""
         return f"UIJson('{self.title if self._path is None else str(self._path.name)}')"
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """String level shows the full json representation."""
         json_string = self.model_dump_json(indent=4, exclude_unset=True)
         return f"{self!r} :\n\n{json_string}"
 
@@ -89,8 +93,18 @@ class BaseUIJson(BaseModel):
         return path
 
     @classmethod
-    def read(cls, path: Path):
-        """Create a UIJson object from file."""
+    def read(cls, path: Path) -> BaseUIJson:
+        """
+        Create a UIJson object from ui.json file.
+
+        Raises errors if the file doesn't exist or is not a .ui.json file.
+        Also validates at the Form and UIJson level whether the file is
+        properly formatted.  If called from the BaseUIJson class, forms
+        will be inferred dynamically.
+
+        :param path: Path to the .ui.json file.
+        :returns: UIJson object.
+        """
 
         path = path.resolve()
 
@@ -126,7 +140,11 @@ class BaseUIJson(BaseModel):
         return uijson
 
     def write(self, path: Path):
-        """Write the UIJson object to file."""
+        """
+        Write the UIJson object to file.
+
+        :param path: Path to write the .ui.json file.
+        """
         self._path = path
         with open(path, "w", encoding="utf-8") as file:
             data = self.model_dump_json(indent=4, exclude_unset=True, by_alias=True)
@@ -134,7 +152,12 @@ class BaseUIJson(BaseModel):
 
     @property
     def groups(self) -> dict[str, list[str]]:
-        """Returns grouped forms."""
+        """
+        Returns grouped forms.
+
+        :returns: Dictionary of group names and the parameters belonging to each
+            group.
+        """
         if self._groups is None:
             groups: dict[str, list[str]] = {}
             for field in self.model_fields:
@@ -152,7 +175,13 @@ class BaseUIJson(BaseModel):
         return self._groups
 
     def is_disabled(self, field: str) -> bool:
-        """Checks if a field is disabled based on form status."""
+        """
+        Checks if a field is disabled based on form status.
+
+        :param field: Field name to check.
+        :returns: True if the field is disabled by it's own enabled status or
+            the groups enabled status, False otherwise.
+        """
 
         value = getattr(self, field)
         if not isinstance(value, BaseForm):
@@ -177,6 +206,10 @@ class BaseUIJson(BaseModel):
 
         :param workspace: Workspace to fetch entities from.  Used for passing active
             workspaces to avoid closing and flushing data.
+
+        :returns: If the data passes validation, to_params returns a promoted and
+            flattened parameters/values dictionary that may be dumped into an application
+            specific params (options) class.
         """
 
         with fetch_active_workspace(workspace or Workspace(self.geoh5)) as geoh5:
@@ -210,7 +243,7 @@ class BaseUIJson(BaseModel):
 
     def validate_data(
         self, params: dict[str, Any] | None = None, errors: dict[str, Any] | None = None
-    ):
+    ) -> None:
         """
         Validate the UIJson data.
 
@@ -245,12 +278,16 @@ class BaseUIJson(BaseModel):
         self,
         workspace: Workspace,
         uuid: UUID,
-    ):
+    ) -> Entity | PropertyGroup | UIJsonError:
         """
         Returns an object if it exists in the workspace or an error if not.
 
         :param workspace: Workspace to fetch entities from.
         :param uuid: UUID of the object to fetch.
+
+        :returns: The object if it exists in the workspace or a placeholder error
+            to be collected and raised later with any other UIJson level validation
+            errors.
         """
 
         obj = workspace.get_entity(uuid)
