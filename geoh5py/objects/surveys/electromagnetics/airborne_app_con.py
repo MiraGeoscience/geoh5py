81e37e3636a0313cba50d63b5b4522e292342e44
dominiquef <dominiquef@mirageoscience.com>
2026-02-02 07:53:56 -0800

    Add classes

---
+++ geoh5py/objects/surveys/electromagnetics/airborne_app_con.py
@@ -0,0 +1,237 @@
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#  Copyright (c) 2020-2026 Mira Geoscience Ltd.                                '
+#                                                                              '
+#  This file is part of geoh5py.                                               '
+#                                                                              '
+#  geoh5py is free software: you can redistribute it and/or modify             '
+#  it under the terms of the GNU Lesser General Public License as published by '
+#  the Free Software Foundation, either version 3 of the License, or           '
+#  (at your option) any later version.                                         '
+#                                                                              '
+#  geoh5py is distributed in the hope that it will be useful,                  '
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              '
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               '
+#  GNU Lesser General Public License for more details.                         '
+#                                                                              '
+#  You should have received a copy of the GNU Lesser General Public License    '
+#  along with geoh5py.  If not, see <https://www.gnu.org/licenses/>.           '
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+from __future__ import annotations
+
+import uuid
+
+import numpy as np
+
+from geoh5py.data import IntegerData, ReferencedData
+from geoh5py.objects.curve import Curve
+from geoh5py.objects.points import Points
+
+from .base import FEMSurvey
+
+
+# pylint: disable=too-many-ancestors
+
+
+class AirborneAppConSurvey(FEMSurvey):
+    """
+    Base tipper survey class.
+    """
+
+    __INPUT_TYPE = ["Rx and base stations"]
+    _base_stations = None
+    _receivers = None
+
+    def __init__(
+        self,
+        base_stations: AirborneAppConBaseStations | None = None,
+        **kwargs,
+    ):
+        self._base_stations = base_stations
+
+        super().__init__(
+            **kwargs,
+        )
+
+    @property
+    def base_stations(self) -> AirborneAppConBaseStations | None:
+        """The base station entity"""
+        if isinstance(self, AirborneAppConBaseStations):
+            return self
+
+        if getattr(self, "_base_stations", None) is None:
+            if (
+                self.metadata is not None
+                and "Base stations" in self.metadata["EM Dataset"]
+            ):
+                base_station = self.metadata["EM Dataset"]["Base stations"]
+                base_station_entity = self.workspace.get_entity(base_station)[0]
+
+                if isinstance(base_station_entity, AirborneAppConBaseStations):
+                    self._base_stations = base_station_entity
+
+        return self._base_stations
+
+    @base_stations.setter
+    def base_stations(self, base: AirborneAppConBaseStations):
+        if not isinstance(base, (AirborneAppConBaseStations, type(None))):
+            raise TypeError(
+                f"Input `base_stations` must be of type '{AirborneAppConBaseStations}' or None"
+            )
+
+        if isinstance(self, AirborneAppConBaseStations):
+            raise AttributeError(
+                f"The 'base_station' attribute cannot be set on class {AirborneAppConBaseStations}."
+            )
+
+        if base.tx_id_property is not None:
+            self.edit_em_metadata({"Tx ID tx property": base.tx_id_property.uid})
+
+            if isinstance(
+                self.tx_id_property, ReferencedData | IntegerData
+            ) and isinstance(base.tx_id_property, ReferencedData | IntegerData):
+                self.tx_id_property.entity_type = base.tx_id_property.entity_type
+
+        self._base_stations = base
+        self.edit_em_metadata({"Base stations": base.uid})
+
+    def copy_from_extent(
+        self,
+        extent: np.ndarray,
+        parent=None,
+        *,
+        copy_children: bool = True,
+        clear_cache: bool = False,
+        inverse: bool = False,
+        **kwargs,
+    ) -> AirborneAppConReceivers | AirborneAppConBaseStations | None:
+        """
+        Sub-class extension of :func:`~geoh5py.shared.entity.Entity.copy_from_extent`.
+        """
+        indices = self.mask_by_extent(extent, inverse=inverse)
+
+        if indices is None:
+            return None
+
+        new_entity = self.copy(
+            parent=parent,
+            copy_children=copy_children,
+            clear_cache=clear_cache,
+            mask=indices,
+            **kwargs,
+        )
+
+        return new_entity
+
+    @property
+    def default_input_types(self) -> list[str]:
+        """Choice of survey creation types."""
+        return self.__INPUT_TYPE
+
+    @property
+    def default_receiver_type(self):
+        """
+        :return: Transmitter class
+        """
+        return AirborneAppConReceivers
+
+    @property
+    def default_transmitter_type(self):
+        """
+        :return: Transmitter class
+        """
+        return type(None)
+
+    @property
+    def base_receiver_type(self):
+        return Curve
+
+    @property
+    def base_transmitter_type(self):
+        return Points
+
+    @property
+    def default_metadata(self) -> dict:
+        """
+        :return: Default unique identifier
+        """
+        return {
+            "EM Dataset": {
+                "Base stations": None,
+                "Channels": [],
+                "Input type": "Rx and base stations",
+                "Property groups": [],
+                "Receivers": None,
+                "Survey type": "Airborne Apparent Conductivity",
+                "Unit": "Hertz (Hz)",
+            }
+        }
+
+    @property
+    def default_units(self) -> list[str]:
+        """Accepted time units. Must be one of "Seconds (s)",
+        "Milliseconds (ms)", "Microseconds (us)" or "Nanoseconds (ns)"
+        """
+        return self.__UNITS
+
+    def _format_transmitter_ids(self, values, attributes):
+        """
+        Format transmitter ids.
+
+        :param values: Array of transmitter ids.
+        :param attributes: Attributes dictionary for the new Data.
+        """
+        if self.complement is not None and self.complement.tx_id_property is not None:
+            attributes["entity_type"] = self.complement.tx_id_property.entity_type
+        else:
+            value_map = {
+                ind: f"Base station {ind}" for ind in np.unique(values.astype(np.int32))
+            }
+            value_map[0] = "Unknown"
+            attributes.update(
+                {
+                    "primitive_type": "REFERENCED",
+                    "value_map": value_map,
+                    "association": "VERTEX",
+                }
+            )
+
+
+class AirborneAppConReceivers(AirborneAppConSurvey, Curve):  # pylint: disable=too-many-ancestors
+    """
+    An airborne apparent conductivity survey object.
+    """
+
+    _TYPE_UID = uuid.UUID("{9f4772d3-92e7-4601-a3c7-23042c2a76ca}")
+    __TYPE = "Receivers"
+    _default_name = "Airborne Apparent Conductivity rx"
+
+    @property
+    def complement(self):
+        return self.base_stations
+
+    @property
+    def type(self):
+        """Survey element type"""
+        return self.__TYPE
+
+
+class AirborneAppConBaseStations(AirborneAppConSurvey, Points):
+    """
+    An airborne apparent conductivity survey object.
+    """
+
+    _TYPE_UID = uuid.UUID("{b389d178-97eb-4ba1-b378-b8b1e8ab35ff}")
+    __TYPE = "Base stations"
+    _default_name = "Airborne Apparent Conductivity base"
+    _minimum_vertices = 1
+
+    @property
+    def complement(self):
+        return self.receivers
+
+    @property
+    def type(self):
+        """Survey element type"""
+        return self.__TYPE
