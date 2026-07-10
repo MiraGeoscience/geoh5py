9297a203b463627f89b40b9927d6256bf3765443
graeme <graemem@mirageoscience.com>
2026-07-10 11:40:01 -0400

    implement giftools h3dtd inversion group. Refactors to reduce complexity with shared code

--- geoh5py/groups/__init__.py
+++ geoh5py/groups/__init__.py
@@ -35,6 +35,7 @@ from .giftools.octree_gravity_inversion import OCTGRVDEInversion
 from .giftools.ip_3d_inversion import IPInv3D
 from .giftools.magnetic_vector_inversion import MVIInv
 from .giftools.e3d_inversion import E3DInv
+from .giftools.h3dtd_inversion import H3DTDInv
 from .group_type import GroupType
 from .integrator import (
     AirborneTheme,
